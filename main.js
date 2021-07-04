17cf487a91f578a646e0b3e7ede32c20e3738e68
md-rakib420 <rayhan.senbag.bd@gmail.com>
2021-07-04 22:15:18 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function input() {
+    var input_taker = document.getElementById("opening").value;
+    document.getElementById("t_opening").innerHTML= input_taker;
+}
+
+
+
+function nput() {
+    var input_taker = document.getElementById("total").value;
+    document.getElementById("t_total").innerHTML= input_taker;
+}
+
+
+function inpu() {
+    var input_taker = document.getElementById("due-sale").value;
+    document.getElementById("t_due-sale").innerHTML= input_taker;
+}
+
+
+
